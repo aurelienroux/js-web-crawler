20f6b08d2c54d240215e315cc7b8ef1df4d07517
Aurélien Roux <aureroux13@gmail.com>
2024-04-27 11:43:30 +0200

    optimize tests

--- crawl.test.js
+++ crawl.test.js
@@ -2,28 +2,28 @@ import { expect, test, describe } from "vitest";
 import { normalizeUrl } from "./crawl.js";
 
 describe("normalize url", () => {
-  test("https url", () => {
+  test("protocol", () => {
     const results = normalizeUrl("https://test.com/path");
     expect(results).toBe("test.com/path");
   });
 
-  test("http url", () => {
-    const results = normalizeUrl("http://test.com/path");
-    expect(results).toBe("test.com/path");
-  });
-
-  test("url with trailing slash", () => {
+  test("trailing slash", () => {
     const results = normalizeUrl("https://test.com/path/");
     expect(results).toBe("test.com/path");
   });
 
-  test("url with with params", () => {
+  test("params", () => {
     const results = normalizeUrl("https://test.com/path?limit=1");
     expect(results).toBe("test.com/path");
   });
 
-  test("url with sub domain", () => {
+  test("subdomain", () => {
     const results = normalizeUrl("https://blog.test.com/path?limit=1");
+    expect(results).toBe("blog.test.com/path");
+  });
+
+  test("capitals", () => {
+    const results = normalizeUrl("https://BLOG.test.com/path?limit=1");
     expect(results).toBe("blog.test.com/path");
   });
 });
