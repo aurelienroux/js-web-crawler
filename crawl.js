d439bd7408e2cb6115553529a64d5479908886f2
Aurélien Roux <aureroux13@gmail.com>
2024-04-27 11:32:09 +0200

    normalize function with TDD

--- crawl.js
+++ crawl.js
@@ -1,3 +1,8 @@
-export function normalizeUrl(url) {
-  return url;
+export function normalizeUrl(entryUrl) {
+  const url = new URL(entryUrl);
+  const pathname = url.pathname.endsWith("/")
+    ? url.pathname.slice(0, -1)
+    : url.pathname;
+
+  return url.host + pathname;
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,10 +1,29 @@
 import { expect, test, describe } from "vitest";
 import { normalizeUrl } from "./crawl.js";
 
-test("normalize url", () => {
-  describe("Name of the group", () => {
-    const results = normalizeUrl("http://test.ca");
+describe("normalize url", () => {
+  test("https url", () => {
+    const results = normalizeUrl("https://test.com/path");
+    expect(results).toBe("test.com/path");
+  });
 
-    expect(results).toBe("test.caa");
+  test("http url", () => {
+    const results = normalizeUrl("http://test.com/path");
+    expect(results).toBe("test.com/path");
+  });
+
+  test("url with trailing slash", () => {
+    const results = normalizeUrl("https://test.com/path/");
+    expect(results).toBe("test.com/path");
+  });
+
+  test("url with with params", () => {
+    const results = normalizeUrl("https://test.com/path?limit=1");
+    expect(results).toBe("test.com/path");
+  });
+
+  test("url with sub domain", () => {
+    const results = normalizeUrl("https://blog.test.com/path?limit=1");
+    expect(results).toBe("blog.test.com/path");
   });
 });
