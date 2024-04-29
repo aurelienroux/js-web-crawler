6658b2e360fd2d73e19239cf31057acd417b915a
Aurélien Roux <aureroux13@gmail.com>
2024-04-29 19:54:29 +0200

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+import { JSDOM } from "jsdom";
+
 export function normalizeUrl(entryUrl) {
   const url = new URL(entryUrl);
   const pathname = url.pathname.endsWith("/")
@@ -5,4 +7,23 @@ export function normalizeUrl(entryUrl) {
     : url.pathname;
 
   return url.host + pathname;
+}
+
+export function getURLsFromHTML(htmlBody, baseURL) {
+  const dom = new JSDOM(htmlBody);
+  const link_nodes_array = Array.from(
+    dom.window.document.querySelectorAll("a")
+  );
+
+  const href_array = link_nodes_array.map((link) => {
+    try {
+      const complete_url = new URL(link.href, baseURL);
+
+      return complete_url.href;
+    } catch (error) {
+      console.log("error converting href:", error);
+    }
+  });
+
+  return href_array;
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,7 +1,7 @@
 import { expect, test, describe } from "vitest";
-import { normalizeUrl } from "./crawl.js";
+import { getURLsFromHTML, normalizeUrl } from "./crawl.js";
 
-describe("normalize url", () => {
+describe("normalizeUrl", () => {
   test("protocol", () => {
     const results = normalizeUrl("https://test.com/path");
     expect(results).toBe("test.com/path");
@@ -25,5 +25,36 @@ describe("normalize url", () => {
   test("capitals", () => {
     const results = normalizeUrl("https://BLOG.test.com/path?limit=1");
     expect(results).toBe("blog.test.com/path");
+  });
+});
+
+describe("getURLsFromHTML", () => {
+  const HtmlFixture = `
+  <html>
+    <body>
+      <p>
+        <a href="/contact.html"><span>Go to Boot.dev</span></a>
+      </p>
+      <a href="https://test.com"><span>Go to Boot.dev</span></a>
+      <aside>
+        <a href="/profile/edit.html"><span>Go to Boot.dev</span></a>
+      </aside>
+    </body>
+  </html>
+  `;
+
+  test("should find all links", () => {
+    const results = getURLsFromHTML(HtmlFixture, "https://test.com");
+
+    expect(results.length).toBe(3);
+  });
+
+  test("should change all relative links to absolute", () => {
+    const pattern = "https://test.com";
+    const results = getURLsFromHTML(HtmlFixture, pattern);
+
+    results.forEach((href) => {
+      expect(href).toMatch(pattern);
+    });
   });
 });
