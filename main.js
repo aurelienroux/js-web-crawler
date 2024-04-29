7750cd0ccc65601702055f654745500e8d721495
Aurélien Roux <aureroux13@gmail.com>
2024-04-29 20:24:41 +0200

    website url user prompt

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("Hello World from main.js!");
+import readline from "node:readline";
+import chalk from "chalk";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function main() {
+  console.log(chalk.blue("*** Welcome to web crawler ***"));
+
+  rl.question(chalk.blue("Enter the website's url: "), (url) => {
+    try {
+      if (url.split(" ").length !== 1) {
+        throw new Error("Please enter only one url");
+      }
+
+      console.log("Starting crawling of:", url);
+      rl.close();
+    } catch (error) {
+      console.log(error);
+      rl.close();
+    }
+  });
+}
+
+main();
